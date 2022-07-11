edd6103c513d73754f461ab6046f4c16bc59106d
paula7026 <108918184+paula7026@users.noreply.github.com>
2022-07-11 09:57:25 +0800

    Create main.js

---
+++ 1.function1/main.js
@@ -0,0 +1,8 @@
+function reverseString(message){
+    // wirte your code here
+    var n=message.split("").reverse().join("");
+    console.log(n);
+
+
+  }
+  reverseString('hello'); // should return 'olleh'
